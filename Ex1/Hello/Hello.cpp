45f5def7cf82a394403fc1ddb3e269b0459e1ea1
ShuGA7 <90182337+ShuGA7@users.noreply.github.com>
2022-09-12 19:00:44 +0300

    Add files via upload

---
+++ Ex1/Hello/Hello.cpp
@@ -0,0 +1,13 @@
+﻿// Hello.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
+
