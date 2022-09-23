5caa8adf3584cba012c5b68998b7588e4ca1473a
ShuGA7 <90182337+ShuGA7@users.noreply.github.com>
2022-09-23 18:39:23 +0300

    Add files via upload

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, char** argv)
+{
+	float temp = 0;
+	int flag = 0;
+	for (size_t i = 0; i < argc; i++)
+	{
+		if (isdigit(*argv[i]))
+		{
+			if (flag == 0)
+				temp = std::stoi(argv[i]);
+			else if (flag == 1)
+			{
+				temp += std::stoi(argv[i]);
+				flag = 0;
+				std::cout << temp << '\n';
+			}
+			else if (flag == 2)
+			{
+				temp -= std::stoi(argv[i]);
+				flag = 0;
+				std::cout << temp << '\n';
+			}
+			else if (flag == 3)
+			{
+				temp /= std::stoi(argv[i]);
+				flag = 0;
+				std::cout << temp << '\n';
+			}
+		}
+		if (!isdigit(*argv[i]))
+		{
+			if (*argv[i] == '+')
+				flag = 1;
+			else if (*argv[i] == '-')
+				flag = 2;
+			else if (*argv[i] == '/')
+				flag = 3;
+		}
+	}
+}
+
+
