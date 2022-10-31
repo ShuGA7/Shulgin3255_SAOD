290defaf005980af67aca36d314c8d618a2e72a8
ShuGA7 <90182337+ShuGA7@users.noreply.github.com>
2022-10-31 11:40:30 +0300

    Add files via upload

---
+++ RefStrings/RefStrings.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include "Str.h"
+using namespace std;
+
+int main()
+{
+	Str s = "123456789";
+	cout << s << endl;
+	Str t = "567";
+	cout << s.rfind(t, 4) << endl;
+}
+
+
---
+++ RefStrings/Str.h
@@ -0,0 +1,66 @@
+#pragma once
+#include "_str.h"
+class Str : public _str
+{
+	_str* m_pStr;
+public:
+	Str() { m_pStr = new _str; }		//если не копия, то создаем
+	Str(const char* p) {		// новый ресурс
+		m_pStr = new _str(p);
+	}
+	~Str() {
+		m_pStr->Release(); 	// Не уничтожаем ресурс!
+	}				// Уменьшаем счетчик ссылок!
+
+	int len() const {
+		return strlen(m_pStr->m_pszData);
+	}
+	int rfind(const char* t, int off) const {
+		int n = strlen(m_pStr->m_pszData), m = strlen(t), j = 0;
+		if (n - m > off)
+			off = off;
+		else
+			off = n - m;
+		for (int i = off; i>=0; i--)
+		{
+			for (j = 0; j < m; j++)
+			{
+				if (m_pStr->m_pszData[i + j] != t[j])
+					break;
+			}
+			if (j == m)
+				return i;
+		}
+		return -1;
+	}
+	Str(const Str& s)
+	{
+		m_pStr = s.m_pStr; 	// ссылаемся на тот же ресурс
+		m_pStr->AddRef(); 	// отмечаем, что сослались
+	}
+	Str& operator = (const Str& s) {
+		if (this != &s) {
+			s.m_pStr->AddRef(); // Важен порядок?!
+			m_pStr->Release();
+			m_pStr = s.m_pStr;
+		}
+		return *this;
+	}
+	Str& operator += (const Str& s) {
+		int length = len() + s.len();
+		if (s.len() != 0) {			// добавление ничего не изменит!
+			_str* pstrTmp = new _str; 	// Новый ресурс
+			delete[] pstrTmp->m_pszData;
+
+			pstrTmp->m_pszData = new char[length + 1];
+			strcpy_s(pstrTmp->m_pszData, length + 1, m_pStr->m_pszData);
+			strcat_s(pstrTmp->m_pszData, length + 1, s.m_pStr->m_pszData);
+
+			m_pStr->Release();
+			m_pStr = pstrTmp;
+		}
+		return *this;
+	}
+	operator const char* ()const { return m_pStr->m_pszData; }
+};
+
